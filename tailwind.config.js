ce4482fb7be4375dd719c40fd1679bb2f670616c
adrishikhar <adrishikhardeka@gmail.com>
2023-10-02 20:55:56 +0530

    First commmit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["*"],
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ["Poppins"],
+				sourceSans: ["Source Sans 3"],
+			},
+		},
+	},
+	plugins: [],
+}
